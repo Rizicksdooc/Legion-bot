0341642dd03f624b590557cb835514fa27b46636
Rizicksdooc <44413540+Rizicksdooc@users.noreply.github.com>
2018-10-23 21:26:15 +0100

    Update normalbot.py

--- normalbot.py
+++ normalbot.py
@@ -9,26 +9,30 @@ bot = commands.Bot(command_prefix="!")
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(game=discord.Game(name = '!help | != prefix'))
+    await bot.change_presence(game=discord.Game(name = 'Helping you. | !help'))
     print("Bot is ONLINE.")
 
 
 #cool thing
 @bot.command(pass_context = True)
 async def rally(ctx):
-    await bot.say("Legion Transmissions presents to you: https://www.roblox.com/games/2504180464/Veil-RALLY ")
+    await bot.say("`Legion Transmissions presents to you `: https://www.roblox.com/games/2504180464/Veil-RALLY ")
 
 @bot.command(pass_context = True)    
 async def holo(ctx):
-    await bot.say(" Legion Transmissions presents to you: https://www.roblox.com/games/2505487043/Sentience-I-TRAIN#!/about ")
+    await bot.say(" `Legion Transmissions presents to you`: https://www.roblox.com/games/2505487043/Sentience-I-TRAIN#!/about ")
 
 @bot.command(pass_context = True)
 async def ping(ctx):
-    await bot.say("PONG :ping_pong: ")    
+    await bot.say("`PONG ` :ping_pong: ")    
 
 
+@bot.command(pass_context = True)
+async def help(ctx):
+    await bot.say("`Commands: !help - What you are using right now! | !ping - Cool little response  |!holo - Our training facility link |  !rally- Our rally center link`")
 
 bot.run("NTA0MzY2NTg5ODY1MzYxNDE5.DrEALA.gEWoWMU7qgiFmYW_uywOjJuVe80")
+
 
 
 
